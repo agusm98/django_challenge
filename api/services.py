f7d959806e7624508c697b4c245983a47a6929fb
agusm98 <agus-e-m1@hotmail.com>
2020-10-14 03:30:13 -0300

    Clase completa ApiCharacter y Database

--- api/services.py
+++ api/services.py
@@ -2,29 +2,31 @@ import requests
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CharacterSerializer
-
+from .models import Rating
 
 def generate_request(url, **kw):
     resp = requests.get(url, params=kw)
     return resp
 
+class Database():
+
+    def insert_rate(self, id, rate):
+        session = Rating(character_id=id, rate=rate)
+        session.save()
+
+    def get_rates(self, id):
+        session = Rating.objects.filter(character_id=id)
+        return [s_obj.rate for s_obj in session]
+
 class ApiCharacter():
 
-    URL = None
+    URL = str()
     response = Response()
+    data = dict()
+    ratings = list()
 
     def __init__(self):
         self.URL = 'http://swapi.dev/api/people/'
-
-    def __get_character(self, char_id):
-        '''Se consume la api de personajes segun id personaje'''
-        return generate_request(self.URL+char_id)
-
-    def __add_average_rating(self):
-        return {'avg_rating': 8}
-
-    def __add_max_rating(self):
-        return {'rating': 8}
 
     @staticmethod
     def __filter_keys(json, fields):
@@ -32,33 +34,85 @@ class ApiCharacter():
 
         return dict(filter(lambda x: x[0] in fields, json.items()))
 
-    def __get_homeworld(self, hw_url):
+    def __get_character(self, char_id):
+        '''Se consume la api de personajes segun id personaje'''
+        self.response = generate_request(self.URL+char_id)
+    
+    def set_ratings(self, char_id):
+        db = Database()
+        self.ratings = db.get_rates(char_id)
 
-        data = generate_request(hw_url).json()
-        data['known_residents_count'] = len(data['residents'])
-
-        data = self.__filter_keys(
-            data, ['known_residents_count', 'population', 'name'])
+    def __add_average_rating(self):
         
-        return data
+        avg = (sum(self.ratings)/len(self.ratings))\
+            if self.ratings\
+                else 0
+        
+        self.data['average_rating'] = avg
+
+    def __add_max_rating(self):
+        self.data['max_rating'] = max(self.ratings, default=0)
+
+    def __get_homeworld(self, hw_key):
+
+        hw_dict = generate_request(self.data[hw_key]).json()
+        hw_dict['known_residents_count'] = len(hw_dict['residents'])
+
+        hw_dict = self.__filter_keys(
+            hw_dict, ['known_residents_count', 'population', 'name'])
+        
+        self.data[hw_key] = hw_dict
+    
+    def __get_species_name(self, species):
+        
+        list_sp = self.data.pop(species)
+        
+        if list_sp: 
+            
+            resp = generate_request(list_sp[0])
+
+            if resp.status_code == 200:
+
+                self.data['species_name'] = resp.json()['name']
+            
+            else:
+                self.data['species_name'] = "Error getting species"
+        
+        else:
+            self.data['species_name'] = "Not Defined"
+
 
     def __parse_response(self):
         '''Hace un request a la url encontrada dentro,
         no es recursiva, no debe ser llamada por otras funciones que se disparen en la consulta
         '''
-        data = self.__filter_keys(self.response.json(), CharacterSerializer.fields)
-        data['homeworld'] = self.__get_homeworld(data['homeworld'])
-        # Se busca el rating
-        data.update(self.__add_average_rating())
-        data.update(self.__add_max_rating())
+        
+        self.data = self.__filter_keys(self.response.json(), CharacterSerializer.fields)
+        
+        self.__get_homeworld('homeworld')
+        
+        self.__get_species_name('species')
 
-        # Se genera la respuesta final
-        self.response = Response(data, status=status.HTTP_200_OK)
+        self.__add_average_rating()
+        
+        self.__add_max_rating()
+    
+    def make_response(self):
+        
+        if self.response.status_code == 200:
+            self.__parse_response()
+            return Response(self.data, status=status.HTTP_200_OK)
+        
+        elif self.response.status_code == 400:
+            return Response(self.response.json(), status=status.HTTP_400_BAD_REQUEST)
 
     def req(self, char_id):
         '''Se realiza un request con la url seteada, y el body pasado por parametro
         '''
-        self.response = self.__get_character(char_id)  # En teoria
-        self.__parse_response()
+        self.__get_character(char_id)
+
+        self.set_ratings(char_id)
+        
+        self.response = self.make_response()
         
         return self.response
